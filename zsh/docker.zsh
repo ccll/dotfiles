a18b645720493856570d731a6a9d72b09fddaf79
ccll <cailei@live.com>
2014-11-22 16:58:19 +0800

    docker: improve docker aliases

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,31 +1,38 @@
 
-# Purge all unactive containers.
-docker-purge-containers () {
-  docker rm $(docker ps -a -q)
-}
-
-# Purge all dangled images.
-docker-purge-images () {
-  images=$(docker images | grep '^<none>' | awk '{print $3}')
+# Prune all unreferenced images.
+docker-prune-images () {
+  images=$(docker images -a | grep '^<none>' | awk '{print $3}')
   if [[ "$images" != "" ]]; then
     docker rmi $images
   fi
 }
 
-# Remove a container.
-docker-rm-container () {
-  if [[ "$1" == "" ]]; then
-    echo "Please specify a container!"
-  else
-    docker kill $1 && docker rm $1
-  fi
+alias dpr="docker-prune-images"
+alias drm="docker rm"
+alias drmi="docker rmi"
+alias di="docker images"
+alias dia="docker images -a"
+alias dc="docker ps"
+alias dca="docker ps -a"
+alias dr="docker run"
+alias db="docker build"
+alias dn="docker inspect"
+alias dnip="docker inspect -f \"{{.NetworkSettings.IPAddress}}\""
+alias dt="docker tag"
+alias de="docker exec"
+alias dcp="docker cp"
+alias dl="docker logs"
+alias did="docker inspect -f \"{{.Id}}\""
+
+# Put files from host to container (the opposite of 'docker cp').
+dput () {
+  local src=$1
+  local container=$2
+  local dst=$3
+  local id=$(docker inspect -f "{{.Id}}" $container)
+  cp -v -r $src /var/lib/docker/aufs/mnt/${id}${dst}
 }
 
-# aliases
-alias dpc="docker-purge-containers"
-alias dpi="docker-purge-images"
-alias drm="docker-rm-container"
-alias di="docker images"
-alias dc="docker ps"
-alias dca="dps -a"
-
+# fig
+alias fup="fig up"
+alias fps="fig ps"
